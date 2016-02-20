d08dbbfb74ee502b5f033c3f689f4c205bef5c81
vinod <vinodiap@gmail.com>
2016-02-20 21:02:05 +0000

    Added comminet in Gi* file
    Change-Id: Ia54d76848077df297f79e517e6e98eadb0e15231
    Signed-off-by: vinod <vinodiap@gmail.com>

--- src/com/atozexample/GitInEclipse.java
+++ src/com/atozexample/GitInEclipse.java
@@ -6,7 +6,9 @@ public class GitInEclipse {
 		// TODO Auto-generated constructor stub
 	}
 
+	//This is the main method
 	public static void main(String[] args) {
+    
 		System.out.println("This is the first git clone project in eclipse");
 
 	}
