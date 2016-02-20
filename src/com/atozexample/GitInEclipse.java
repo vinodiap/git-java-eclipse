bdb5667a610c59e102753f9ece4b3eadfd6aedf9
vinod <vinodiap@gmail.com>
2016-02-20 21:00:42 +0000

    Mod file for added main in sysout.
    Change-Id: Ib79f0e802556955888b5047f039ab62fb3c20840
    Signed-off-by: vinod <vinodiap@gmail.com>

--- src/com/atozexample/GitInEclipse.java
+++ src/com/atozexample/GitInEclipse.java
@@ -7,7 +7,7 @@ public class GitInEclipse {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is the first git clone project in eclipse");
 
 	}
 
