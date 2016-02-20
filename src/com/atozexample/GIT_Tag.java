8752cd4567bbfc52c92f24a594ab0dd35bcc9dcf
vinod <vinodiap@gmail.com>
2016-02-20 21:12:43 +0000

    This is for tag
    Change-Id: I6336d0bda2de53ebf395046152a38ae011bd637a
    Signed-off-by: vinod <vinodiap@gmail.com>

---
+++ src/com/atozexample/GIT_Tag.java
@@ -0,0 +1,14 @@
+package com.atozexample;
+
+public class GIT_Tag {
+
+	public GIT_Tag() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
